29569f055851e5e22c4dac1c4208b60e6dcaea30
Mohammed Ismail <m.ismail.x2@gmail.com>
2026-08-05 09:23:30 +0200

    Initial commit

---
+++ restaurant/views.py
@@ -0,0 +1,19 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import BookingForm
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
