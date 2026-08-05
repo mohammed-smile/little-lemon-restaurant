e5b11406644e8b7dd359607b3ee2558c1abbf0ae
Mohammed Ismail <m.ismail.x2@gmail.com>
2026-08-05 22:08:50 +0200

    feat: add django debug toolbar

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from config import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
