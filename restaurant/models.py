cd7261b7369fd061ffe59690958390ee043bedf4
Mohammed Ismail <m.ismail.x2@gmail.com>
2026-08-05 23:22:14 +0200

    feat: add the menu model

--- restaurant/models.py
+++ restaurant/models.py
@@ -9,3 +9,11 @@ class Booking(models.Model):
 
    def __str__(self):
       return self.first_name + ' ' + self.last_name
+
+
+class Menu(models.Model):
+   name = models.CharField(max_length=200)
+   price = models.IntegerField()
+
+   def __str__(self):
+      return self.name
