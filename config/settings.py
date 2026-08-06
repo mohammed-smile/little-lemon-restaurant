1301912602dcac0119cb58addda2ffbe24bde0f0
Mohammed Ismail <m.ismail.x2@gmail.com>
2026-08-06 12:14:06 +0200

    chore: add DEBUG level logging

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,21 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
